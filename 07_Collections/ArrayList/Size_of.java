e2240eb49d77c0fc722bacccb4ba4abd563e3469
Sanika Kaminwar <kaminwarsanika@gmail.com>
2025-09-14 11:34:45 +0530

    Update Size_of.java

--- 07_Collections/ArrayList/Size_of.java
+++ 07_Collections/ArrayList/Size_of.java
@@ -1,5 +1,10 @@
-c
+import java.util.*;
 
+class Size_of{
+        public static void main (String [] args)
+        {
+        ArrayList <String> list=new ArrayList<>();
+        list.add("Sanika");
         System.out.println(list.size());
         list.add("Shraddha J");
         System.out.println(list.size());
